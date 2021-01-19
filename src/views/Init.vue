e0de1c70c2be655a21d61770068877aff7f11414
吴诚骏 <cjwu@che300.com>
2021-01-19 16:47:05 +0800

    init

---
+++ src/views/Init.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-card style="width: 500px">
+    <div slot="header">
+      <span>首次使用配置</span>
+    </div>
+    <el-form>
+      <el-form-item>
+      </el-form-item>
+    </el-form>
+  </el-card>
+</template>
+
+<script>
+export default {
+  name: 'Init',
+  data () {
+    return {
+      form: {
+        AesKey: '',
+        Username: '',
+        Password: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    }
+  }
+}
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="bg d-flex justify-content-center align-items-center">
+    <div class="container p-3">
+      <div class="p-1" style="height: 33.3333%">
+        <el-input size="small" v-model="form.username" suffix-icon="el-icon-user" />
+      </div>
+      <div class="p-1" style="height: 33.3333%">
+        <el-input size="small" v-model="form.password" type="password" suffix-icon="el-icon-lock" />
+      </div>
+      <div class="p-1" style="height: 33.3333%">
+        <el-button type="primary" size="small" class="w-100">登陆</el-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      form: {
+        username: '',
+        password: ''
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.bg{
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  background: url("../assets/bg.jpg") no-repeat;
+  background-size: cover;
+}
+.container{
+  width: 300px;
+  height: 188px;
+  background: #eee;
+  border-radius: 5px;
+  opacity: 0.8;
+}
+</style>
